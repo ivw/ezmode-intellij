637f36fa39801bc27f9bda8ab9f7934ed822be0a
Ivo <ivowijnker@hotmail.com>
2025-04-24 19:20:08 -0400

    formatting

--- src/main/kotlin/com/github/ivw/ezmode/MyBundle.kt
+++ src/main/kotlin/com/github/ivw/ezmode/MyBundle.kt
@@ -1,20 +1,18 @@
 package com.github.ivw.ezmode
 
-import com.intellij.DynamicBundle
-import org.jetbrains.annotations.NonNls
-import org.jetbrains.annotations.PropertyKey
+import com.intellij.*
+import org.jetbrains.annotations.*
 
 @NonNls
 private const val BUNDLE = "messages.MyBundle"
 
 object MyBundle : DynamicBundle(BUNDLE) {
 
-    @JvmStatic
-    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-        getMessage(key, *params)
+  @JvmStatic
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+    getMessage(key, *params)
 
-    @Suppress("unused")
-    @JvmStatic
-    fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
-        getLazyMessage(key, *params)
+  @JvmStatic
+  fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
+    getLazyMessage(key, *params)
 }
