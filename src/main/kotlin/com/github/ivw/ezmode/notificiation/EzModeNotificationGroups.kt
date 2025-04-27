d6bea94244ac4f45a43e2699e7ee379101b6a628
Ivo <ivowijnker@hotmail.com>
2025-04-27 13:07:27 -0400

    extract ParserNotifications.notifyError

--- src/main/kotlin/com/github/ivw/ezmode/notificiation/EzModeNotificationGroups.kt
+++ src/main/kotlin/com/github/ivw/ezmode/notificiation/EzModeNotificationGroups.kt
@@ -1,10 +1,25 @@
 package com.github.ivw.ezmode.notificiation
 
+import com.github.ivw.ezmode.*
+import com.github.ivw.ezmode.keymap.*
 import com.intellij.notification.*
+import java.io.*
 
-object EzModeNotificationGroups {
-  val parser: NotificationGroup? by lazy {
+object ParserNotifications {
+  val group: NotificationGroup by lazy {
     NotificationGroupManager.getInstance()
       .getNotificationGroup("ezmode.parser")
+  }
+
+  fun notifyError(file: File, e: Throwable) {
+    val title = EzModeBundle.message("notificationGroup.ezmode.parser.failed.title", file.name)
+    val content: String = if (e is EzModeRcParser.ParseError) {
+      e.toNiceString()
+    } else {
+      e.message ?: e.javaClass.name
+    }
+    group.createNotification(
+      title, content, NotificationType.WARNING
+    ).notify(null)
   }
 }
