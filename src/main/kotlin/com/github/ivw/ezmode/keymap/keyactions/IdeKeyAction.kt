d92c0a2161c0d6b36b3d935185dfa638eeb5d953
Ivo <ivowijnker@hotmail.com>
2025-04-29 21:30:23 -0400

    log when IDE action is not found

--- src/main/kotlin/com/github/ivw/ezmode/keymap/keyactions/IdeKeyAction.kt
+++ src/main/kotlin/com/github/ivw/ezmode/keymap/keyactions/IdeKeyAction.kt
@@ -2,9 +2,13 @@ package com.github.ivw.ezmode.keymap.keyactions
 
 import com.github.ivw.ezmode.keymap.*
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.diagnostic.*
 
 data class IdeKeyAction(val actionId: String) : KeyAction() {
-  val anAction: AnAction? by lazy { ActionManager.getInstance().getAction(actionId) }
+  val anAction: AnAction? by lazy {
+    ActionManager.getInstance().getAction(actionId)
+      .also { if (it == null) thisLogger().info("Action $actionId not found") }
+  }
 
   override fun perform(e: EzModeKeyEvent) {
     anAction?.let { anAction ->
