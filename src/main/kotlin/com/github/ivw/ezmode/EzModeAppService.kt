f621ce1c2c70f8aae30fa3006c6f65bba538d58d
Ivo <ivowijnker@hotmail.com>
2025-04-30 11:36:21 -0400

    add CheatSheet tool window

--- src/main/kotlin/com/github/ivw/ezmode/EzModeAppService.kt
+++ src/main/kotlin/com/github/ivw/ezmode/EzModeAppService.kt
@@ -3,14 +3,18 @@ package com.github.ivw.ezmode
 import com.github.ivw.ezmode.editor.*
 import com.github.ivw.ezmode.keymap.*
 import com.intellij.openapi.*
+import com.intellij.openapi.application.*
 import com.intellij.openapi.components.*
 import com.intellij.openapi.diagnostic.*
 import com.intellij.openapi.editor.*
 import com.intellij.openapi.editor.actionSystem.*
+import com.intellij.util.messages.*
 
 @Service
 class EzModeAppService : Disposable {
   private var keyMap: MutableEzModeKeyMap? = null
+
+  val application: Application = ApplicationManager.getApplication()
 
   override fun dispose() {
     LOG.info("App dispose")
@@ -35,11 +39,33 @@ class EzModeAppService : Disposable {
   }
 
   fun loadKeymap(): MutableEzModeKeyMap =
-    EzModeRcFileUtils.getKeyMap().also { keyMap = it }
+    EzModeRcFileUtils.getKeyMap().also {
+      keyMap = it
+      application.messageBus.syncPublisher(KEYMAP_CHANGE_TOPIC)
+        .onChanged(it)
+    }
 
   fun getKeyMap(): MutableEzModeKeyMap = keyMap ?: loadKeymap()
 
+  fun subscribeToKeyMap(parentDisposable: Disposable, onChange: (MutableEzModeKeyMap) -> Unit) {
+    application.messageBus.connect(parentDisposable).subscribe(
+      KEYMAP_CHANGE_TOPIC,
+      object : KeyMapChangeNotifier {
+        override fun onChanged(keyMap: MutableEzModeKeyMap) {
+          onChange(keyMap)
+        }
+      }
+    )
+  }
+
+  interface KeyMapChangeNotifier {
+    fun onChanged(keyMap: MutableEzModeKeyMap)
+  }
+
   companion object {
     private val LOG = logger<EzModeAppService>()
+
+    @Topic.AppLevel
+    val KEYMAP_CHANGE_TOPIC = Topic(KeyMapChangeNotifier::class.java)
   }
 }
---
+++ src/main/kotlin/com/github/ivw/ezmode/cheatsheet/CheatSheetToolWindowFactory.kt
@@ -0,0 +1,42 @@
+package com.github.ivw.ezmode.cheatsheet
+
+import com.github.ivw.ezmode.*
+import com.github.ivw.ezmode.editor.*
+import com.github.ivw.ezmode.keymap.*
+import com.intellij.openapi.components.*
+import com.intellij.openapi.editor.colors.*
+import com.intellij.openapi.project.*
+import com.intellij.openapi.wm.*
+import com.intellij.ui.components.*
+import com.intellij.ui.content.*
+import javax.swing.*
+
+class CheatSheetToolWindowFactory : ToolWindowFactory, DumbAware {
+  override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+    val appService = service<EzModeAppService>()
+    val editorFont = EditorColorsManager.getInstance().globalScheme.getFont(EditorFontType.PLAIN)
+    val textArea = JTextArea().apply {
+      isEditable = false
+      lineWrap = true
+      wrapStyleWord = true
+      font = editorFont
+    }
+
+    fun updateText() {
+      textArea.text = appService.getKeyMap().toNiceString(Mode.EZ)
+    }
+    updateText()
+
+    val component = JBScrollPane(textArea)
+    val content = ContentFactory.getInstance().createContent(
+      component,
+      null,
+      false
+    )
+    toolWindow.contentManager.addContent(content)
+
+    appService.subscribeToKeyMap(content) { _ ->
+      updateText()
+    }
+  }
+}
