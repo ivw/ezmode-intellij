5d7d9a8558e88dc46842b01d58104bc656ca7515
Ivo <ivowijnker@hotmail.com>
2025-05-06 13:22:31 -0400

    add diff navigation keys

--- src/main/kotlin/com/github/ivw/ezmode/config/keyactions/IdeKeyAction.kt
+++ src/main/kotlin/com/github/ivw/ezmode/config/keyactions/IdeKeyAction.kt
@@ -1,8 +1,11 @@
 package com.github.ivw.ezmode.config.keyactions
 
 import com.github.ivw.ezmode.config.*
+import com.intellij.ide.*
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.diagnostic.*
+import java.awt.event.*
+import javax.swing.*
 
 data class IdeKeyAction(val actionId: String) : KeyAction() {
   val anAction: AnAction? by lazy {
@@ -12,17 +15,42 @@ data class IdeKeyAction(val actionId: String) : KeyAction() {
 
   override fun perform(e: EzModeKeyEvent) {
     anAction?.let { anAction ->
-      val presentation = Presentation()
-      val anActionEvent = AnActionEvent.createFromDataContext(
-        "ezmode", presentation, e.dataContext
-      )
-      anAction.update(anActionEvent)
-      if (presentation.isEnabled) {
-        anAction.actionPerformed(anActionEvent)
+      if (anAction is EmptyAction) {
+        handleEmptyAction(anAction, e)
       } else {
-        LOG.info("Action not enabled: $actionId")
+        val presentation = Presentation()
+        val anActionEvent = AnActionEvent.createFromDataContext(
+          "ezmode", presentation, e.dataContext
+        )
+        anAction.update(anActionEvent)
+        if (presentation.isEnabled) {
+          anAction.actionPerformed(anActionEvent)
+        } else {
+          LOG.info("Action not enabled: $actionId")
+        }
       }
     }
+  }
+
+  /**
+   * Some actions (e.g. NextDiff) are defined globally as an EmptyAction,
+   * with the real action added locally to some context, such as a diff viewer.
+   * This is a workaround that simulates the keystroke defined in the EmptyAction.
+   */
+  fun handleEmptyAction(anAction: EmptyAction, e: EzModeKeyEvent) {
+    val keyStroke = anAction.getKeyStroke() ?: run {
+      LOG.info("Action is empty and has no keyboard shortcut: $actionId")
+      return
+    }
+    val event = KeyEvent(
+      e.editor.contentComponent,
+      KeyEvent.KEY_PRESSED,
+      System.currentTimeMillis(),
+      keyStroke.modifiers,
+      keyStroke.keyCode,
+      KeyEvent.CHAR_UNDEFINED
+    )
+    IdeEventQueue.getInstance().dispatchEvent(event)
   }
 
   override fun toNiceString(): String =
@@ -33,4 +61,13 @@ data class IdeKeyAction(val actionId: String) : KeyAction() {
   companion object {
     val LOG = logger<IdeKeyAction>()
   }
+}
+
+fun AnAction.getKeyStroke(): KeyStroke? {
+  for (shortcut in shortcutSet.shortcuts) {
+    if (shortcut is KeyboardShortcut && shortcut.secondKeyStroke == null) {
+      return shortcut.firstKeyStroke
+    }
+  }
+  return null
 }
