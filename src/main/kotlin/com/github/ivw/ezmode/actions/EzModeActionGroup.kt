6801953b45f42c55ad3fe5fbf22500bb6d4b4fcf
Ivo <ivowijnker@hotmail.com>
2025-05-25 12:53:08 -0400

    add EzModeActionGroupPopup action that always uses the right ActionSelectionAid

--- src/main/kotlin/com/github/ivw/ezmode/actions/EzModeActionGroup.kt
+++ src/main/kotlin/com/github/ivw/ezmode/actions/EzModeActionGroup.kt
@@ -4,19 +4,16 @@ import com.github.ivw.ezmode.*
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.impl.*
 import com.intellij.openapi.editor.*
+import com.intellij.openapi.project.*
 import com.intellij.openapi.ui.popup.*
 
 class EzModeActionGroup : DefaultActionGroup() {
-  override fun actionPerformed(e: AnActionEvent) {
-    createPopup(e.dataContext).showInBestPositionFor(e.dataContext)
-  }
-
   fun createPopup(dataContext: DataContext) =
     JBPopupFactory.getInstance().createActionGroupPopup(
       EzModeBundle.message("group.ezmode.EzModeActionGroup.text"),
       this,
       dataContext,
-      JBPopupFactory.ActionSelectionAid.ALPHA_NUMBERING,
+      JBPopupFactory.ActionSelectionAid.NUMBERING,
       false,
     )
 
@@ -28,4 +25,11 @@ class EzModeActionGroup : DefaultActionGroup() {
         .build()
     } ?: DataContext.EMPTY_CONTEXT
   )
+
+  class PopupAction : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+      (ActionManager.getInstance().getAction("ezmode.EzModeActionGroup") as? EzModeActionGroup)
+        ?.createPopup(e.dataContext)?.showInBestPositionFor(e.dataContext)
+    }
+  }
 }
