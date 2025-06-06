7f67283e57aa479724fb6cb4e7e1ee09d0954f30
Ivo <ivowijnker@hotmail.com>
2025-06-06 11:27:11 -0400

    SelectLineAction does not select whitespace at the start of the line

--- src/main/kotlin/com/github/ivw/ezmode/actions/SelectLineAction.kt
+++ src/main/kotlin/com/github/ivw/ezmode/actions/SelectLineAction.kt
@@ -3,15 +3,19 @@ package com.github.ivw.ezmode.actions
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.editor.*
 import com.intellij.openapi.editor.actionSystem.*
+import com.intellij.openapi.editor.actions.*
 
 class SelectLineAction : EditorAction(Handler()) {
   class Handler : EditorActionHandler.ForEachCaret() {
     override fun doExecute(editor: Editor, caret: Caret, dataContext: DataContext?) {
-      val start = editor.visualPositionToOffset(VisualPosition(caret.selectionStartPosition.line, 0))
-      val end = editor.visualPositionToOffset(caret.selectionEndPosition.line.let { line ->
-        VisualPosition(line, editor.document.getLineEndOffset(line) - 1)
+      val start = editor.visualPositionToOffset(caret.selectionStartPosition.line.let { line ->
+        val i = EditorActionUtil.findFirstNonSpaceColumnOnTheLine(editor, line)
+        VisualPosition(line, if (i == -1) 0 else i)
       })
-      caret.moveToOffset(start)
+      val end = editor.visualPositionToOffset(caret.selectionEndPosition.line.let { line ->
+        VisualPosition(line, Integer.MAX_VALUE)
+      })
+      caret.moveToOffset(end)
       caret.setSelection(start, end)
     }
   }
